36a055d795ed6a2917906e70f840a4ff5ef9f77a
chiefEbube <ebubechi.anyanwu@yahoo.com>
2026-02-05 19:31:43 +0100

    feat: add ScrollToTop component to enhance navigation experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/layout/Navbar";
 // import Footer from "@/components/layout/Footer";
 import FooterBottom from "./components/layout/FooterBottom";
+import ScrollToTop from "@/components/ScrollToTop";
 import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <main>
         <Outlet />
