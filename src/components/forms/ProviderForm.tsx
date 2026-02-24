2c67b0a73a193a6cfd89ab222e053e166629e053
chiefEbube <ebubechi.anyanwu@yahoo.com>
2026-02-24 15:10:01 +0100

    feat: update introductory text in ProviderForm for improved clarity and personalization

--- src/components/forms/ProviderForm.tsx
+++ src/components/forms/ProviderForm.tsx
@@ -105,7 +105,7 @@ const form = useForm<ProviderFormValues>({
           </h2>
 
           <p className="mt-4.5 text-[#0D0F11] mx-auto font-medium max-w-2xl text-lg md:text-xl leading-relaxed">
-            {`Fill out the form below to apply as a service provider. We will review applications and get back to you with the next steps.`}
+            {`Fill out the form below to apply as a service provider. We will review your application and get back to you with the next steps.`}
           </p>
         </div>
 
